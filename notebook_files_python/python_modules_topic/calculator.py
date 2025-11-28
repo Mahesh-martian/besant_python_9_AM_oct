941c9c7224eefbfff646850f454b2e44f3c72221
Mahesh kumar.R <54738956+Mahesh-martian@users.noreply.github.com>
2025-11-28 09:41:21 +0530

    adding modules topic

---
+++ notebook_files_python/python_modules_topic/calculator.py
@@ -0,0 +1,14 @@
+print("This is calculator module")
+
+a = 10
+b = 20
+
+def add(a,b):
+    print("This is calculator add")
+    print(a+b)
+
+def sub(a,b):
+    print(a-b)
+
+def div(a,b):
+    print(a/b)
---
+++ notebook_files_python/python_modules_topic/person.py
@@ -0,0 +1,34 @@
+# import calculator
+
+# print(calculator.a)
+# calculator.add(1,2)
+
+
+# from calculator import *
+
+# print(a)
+# add(1,2)
+
+
+# from calculator import a, add
+
+# # print(b)
+
+# def add(a,b):
+#     print("This is person add")
+#     print(a+b)
+
+
+# add(100,200)
+
+from calculator import a, add as calcadd
+
+# print(b)
+
+def add(a,b):
+    print("This is person add")
+    print(a+b)
+
+
+add(100,200)
+calcadd(100,200)
